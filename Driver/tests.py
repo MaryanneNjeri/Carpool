3f249157da156dadf0229495a2214ca9f91759aa
MaryAnne45 <mary.jereh@gmail.com>
2018-05-29 12:00:29 +0300

    added a test to check if the object is created

--- Driver/tests.py
+++ Driver/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import Profile,Car,Driver,Passenger,Venue
+from django.contrib.auth.models import User
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    #set Up the method
+
+    def setUp(self):
+        user1=User(first_name="Suzan",last_name="Wanja")
+        self.person1=Profile(username='Suzan',user_type='Driver',sex='Female',user=user1)
+    #Testing whether the instance has been created
+    def test_instance(self):
+        self.assertTrue(isinstance(self.person1,Profile))
